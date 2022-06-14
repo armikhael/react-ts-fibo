bd1ce60c80dd6277c98ee4a233d0642dcc2aefb3
armikhael <carlosarmikhael@gmail.com>
2022-06-14 13:56:46 -0400

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import './style.css';
+
+export default function App() {
+  const [array, setArray] = useState<number[]>();
+  const [inputs, setInputs] = useState<any>({});
+
+  const handleFib = (number: number) => {
+    let fib = [0, 1];
+    for (let i = 2; i <= number; i++) {
+      fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    setArray(fib);
+  };
+
+  const handleChange = (event) => {
+    setInputs((values) => ({
+      ...values,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    handleFib(inputs.number);
+  };
+
+  useEffect(() => {
+    handleFib(5);
+  }, []);
+
+  return (
+    <div>
+      <h1>Sucesión de Fibonacci!</h1>
+      <p>Ingrese un numero :) </p>
+      <form onSubmit={handleSubmit}>
+        <input
+          required
+          type="number"
+          name="number"
+          value={inputs.number}
+          onChange={handleChange}
+        />
+        <input type="submit" />
+      </form>
+      {array && (
+        <pre>
+          <ul>
+            {array.map((item, index) => {
+              return <li key={index}>{item}</li>;
+            })}
+          </ul>
+        </pre>
+      )}
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-rrevnt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-rrevnt)
